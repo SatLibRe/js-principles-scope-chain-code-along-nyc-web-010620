5a6bab5d3e024db57deb92469d870a16598f8b77
Miles Higbie <miles.higbie@gmail.com>
2020-02-16 18:53:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-/* Your code goes here */
+customerName = "bob"
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+ bestCustomer = "not bob" 
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "miles"
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "danyelle"
+}
